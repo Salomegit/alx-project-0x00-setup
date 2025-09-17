8a374a1feee149373a281ce5bb687c2b89ec2d4f
Salomegit <sallygithinji72@gmail.com>
2025-09-17 13:24:31 +0300

    styling added

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+  styles?: string; // tailwind classes passed as string
+}
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`px-4 py-2 font-medium text-white bg-blue-500 hover:bg-blue-600 transition-colors ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Card from '@/components/Card'
+import Button from '@/components/Button'
 
 const landing:React.FC = () => {
   return (
@@ -8,6 +9,15 @@ const landing:React.FC = () => {
 
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+
+       <div className="flex gap-4">
+        <Button title="Small Rounded-sm" styles="text-sm rounded-sm px-2 py-1" />
+        <Button title="Small Rounded-md" styles="text-sm rounded-md px-2 py-1" />
+        <Button
+          title="Small Rounded-full"
+          styles="text-sm rounded-full px-2 py-1"
+        />
+      </div>
     </div>
 
   )
